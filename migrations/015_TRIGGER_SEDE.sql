8077a107a3c773d6de5ef402f7677b4cf4ab6488
Arturo Pérez <arjperez31@gmail.com>
2025-01-11 17:52:29 -0400

    feat: añadiendo lógica crear solicitud cliente

--- migrations/015_TRIGGER_SEDE.sql
+++ migrations/015_TRIGGER_SEDE.sql
@@ -23,10 +23,42 @@ EXECUTE FUNCTION verificar_cantidad_stock();
 
 CREATE OR REPLACE FUNCTION enviar_solicitud_proveedor()
 RETURNS TRIGGER AS $$
+DECLARE
+	proveedor_id INT;
 BEGIN
+
+	SELECT p.com_id
+	INTO proveedor_id
+	FROM 
+		proveedor p, 
+		proveedor_mp_stock ps
+	WHERE 
+		p.fk_mtp_id = ps.mtp_id
+		AND
+		ps.fk_rpm_id = NEW.fk_rpm_id;
+		
     IF NEW.mps_cantidad_disponible < 150 THEN
-        -- Aquí puedes añadir el código para enviar la solicitud
-        RAISE NOTICE 'Solicitud enviada porque la cantidad de material es %', NEW.mps_cantidad_disponible;
+		INSERT INTO SOLICITUD_PROVEEDOR 
+		(
+			spr_fecha, spr_total, 
+			spr_descripcion, 
+			fk_com_id, 
+			fk_mps_id, 
+			fk_sed_id, 
+			fk_rpm_id
+		)
+		VALUES 
+		(
+			CURRENT_DATE, 
+		 	0, 
+			(SELECT 'Solicitud de' || rpm_nombre
+			FROM materia_prima 
+			WHERE rpm_id = NEW.fk_rpm_id),
+			proveedor_id,
+			NEW.mps_id,
+			NEW.fk_sed_id,
+			NEW.fk_rpm_id
+		);
     END IF;
     RETURN NEW;
 END;
